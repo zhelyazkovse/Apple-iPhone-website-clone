0cd87f34fd8fe6302fac92e7a38926224a09fd4f
zhelyazkovse <zhelyazkovse@gmail.com>
2025-02-15 14:34:36 +0100

    Secondary Deployment

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), sentryVitePlugin({
+    org: "jsm-ih",
+    project: "javascript-react"
+  }), sentryVitePlugin({
+    org: "jsm-ih",
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
+})
